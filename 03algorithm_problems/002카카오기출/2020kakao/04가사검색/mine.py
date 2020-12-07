775ed2970eec32984aa92b3af33b94add2e99dad
taelee <taelee@student.42seoul.kr>
2020-12-07 16:30:22 +0900

    algorithm_problem(가사검색): add ndb solution and mine

--- 03algorithm_problems/002카카오기출/2020kakao/04가사검색/mine.py
+++ 03algorithm_problems/002카카오기출/2020kakao/04가사검색/mine.py
@@ -1,4 +1,5 @@
-from bisect import bisect_left, bisect_right
+from bisect import bisect_right, bisect_left
+
 
 def count_range(array, left_value, right_value):
     left_index = bisect_left(array, left_value)
@@ -6,15 +7,28 @@ def count_range(array, left_value, right_value):
     return right_index - left_index
 
 
-
 def solution(words, queries):
     words_by_length = [[] for _ in range(10001)]
-    test = [[0]*20]
-    print("test", test)
-    test[0][0] = 1
-    print("test", test)
-    print("here", words_by_length)
+    words_by_length_reverse = [[] for _ in range(10001)]
+    result = []
 
+    for word in words:
+        words_by_length[len(word)].append(word)
+        words_by_length_reverse[len(word)].append(word[::-1])
+    for group in words_by_length:
+        group.sort()
+    for group in words_by_length_reverse:
+        group.sort()
+
+    for query in queries:
+        if query[-1] == '?':
+            result.append(count_range(words_by_length[len(query)], query.replace('?', 'a'), query.replace('?', 'z')))
+        elif query[0] == '?':
+            result.append(count_range(words_by_length_reverse[len(query)], query.replace('?', 'a')[::-1],
+                                      query.replace('?', 'z')[::-1]))
+        else:
+            result.append(1 if count_range(words_by_length[len(query)]).contains(query) else 0)
+    return result
 
 
 words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
